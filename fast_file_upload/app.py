b15327e95c0552ad9547e3057a0f0ac02deead57
Bussler <maarten.bussler@gmail.com>
2024-08-19 21:56:44 +0200

    add streamed file upload

--- fast_file_upload/app.py
+++ fast_file_upload/app.py
@@ -1,5 +1,10 @@
+import os
+
+import aiofiles
 import uvicorn
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, HTTPException, Request, UploadFile, status
+from streaming_form_data import StreamingFormDataParser
+from streaming_form_data.targets import FileTarget
 
 app = FastAPI()
 
@@ -9,12 +14,49 @@ async def say_hello() -> str:
     return "Success! Connection is working."
 
 
-@app.post("/")
-async def file_upload(
+@app.post("/small_file_upload/")
+async def small_file_upload(
     file: UploadFile,
 ) -> str:
+    """Read file in as Upload file and save it to a file async.
 
-    file_contents = file.file.read()
+    Pro: Get good swagger documentation with UploadFile.
+    Con: Slow, since Upload File is spooled and has to be loaded to memory first before saving.
+    """
+
+    file_contents = await file.read()
+    async with aiofiles.open(f"{file.filename}", "wb") as out_file:
+        await out_file.write(file_contents)
+    return "Success! File uploaded."
+
+
+@app.post("/large_file_upload/")
+async def large_file_upload(request: Request) -> str:
+    """Read filedata as a stream from requests and save it to a file.
+    Pro: Stream data directly to file, so no memory overhead.
+    Con: Bad documentation and error handling, since data has to be parsed from request body.
+    """
+
+    filename = request.headers.get("Filename")
+
+    if not filename:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Filename header is missing"
+        )
+
+    try:
+        filepath = os.path.join("./", os.path.basename(filename))
+        file_ = FileTarget(filepath)  # stream to load into file
+        # data = ValueTarget() # stream to load into memory
+        parser = StreamingFormDataParser(headers=request.headers)
+        parser.register("file", file_)
+        # parser.register('data', data)
+        async for chunk in request.stream():
+            parser.data_received(chunk)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+    print(file_.multipart_filename)
     return "Success! File uploaded."
 
 
--- fast_file_upload/file_upload.py
+++ fast_file_upload/file_upload.py
@@ -6,8 +6,8 @@ from fast_file_upload import logger
 BASE_URL = "http://localhost:8001"
 
 
-# Fastest way to upload large files
-def upload_zip_file(path_to_zip: str) -> None:
+# Fastest way to upload small files
+def upload_zip_file_small(path_to_zip: str) -> None:
     """Automatically create multipart formdata by reading the file as a stream and passing to the files field of the post request.
 
     Args:
@@ -15,12 +15,12 @@ def upload_zip_file(path_to_zip: str) -> None:
     """
     with open(path_to_zip, "rb") as f:
         create_response = requests.post(
-            f"{BASE_URL}/",
+            f"{BASE_URL}/small_file_upload/",
             files=[
                 (
                     "file",
                     (
-                        "zip",
+                        "file.zip",
                         f,
                         "application/x-zip-compressed",
                     ),
@@ -31,25 +31,27 @@ def upload_zip_file(path_to_zip: str) -> None:
         logger.info(f"Success: {create_response.text}")
 
 
-# Best way to upload large files with less memory usage
-def upload_zip_multipart_encoder(path_to_zip: str) -> None:
-    """Create multipart formdata to pass to the data field of the post request.
+# Fastest way to upload large files
+def upload_zip_file_big(path_to_zip: str) -> None:
+    """Automatically create multipart formdata by reading the file as a stream and passing to the files field of the post request.
 
     Args:
         path_to_zip (str): path to the zip file to upload.
     """
-    encoder = MultipartEncoder(
-        fields={"file": ("zip", open(path_to_zip, "rb"), "application/x-zip-compressed")}
-    )
-    response = requests.post(
-        f"{BASE_URL}/", data=encoder, headers={"Content-Type": encoder.content_type}
-    )
-    logger.info(f"Success: {response.text}")
+    with open(path_to_zip, "rb") as f:
+        create_response = requests.post(
+            f"{BASE_URL}/large_file_upload/",
+            files={"file": f},
+            headers={"Filename": "bigFile.zip"},
+        )
+
+        logger.info(f"Success: {create_response.text}")
 
 
 def main() -> None:
-    # upload_zip_file("test.zip")
-    upload_zip_multipart_encoder("test.zip")
+    upload_zip_file_small("data/test.zip")
+    # upload_zip_file_big("data/old.zip")
+    # upload_zip_multipart_encoder("test.zip")
 
 
 if __name__ == "__main__":
